8d2fb9c164a1b32651bcd8ed4ebb14cfa82b8b50
vincloud2 <128999303+vincloud2@users.noreply.github.com>
2023-08-09 23:56:34 +0530

    Update whitelist.ps1

--- whitelist.ps1
+++ whitelist.ps1
@@ -1,16 +1,27 @@
 # Specify the root directory where you want to start the scan
 $rootDirectory = "C:\"
 
+# List of directories to exclude (add more if needed)
+$excludedDirectories = @(
+    "C:\whitelist",
+    "C:\whitelist"
+)
+
 # Hash algorithm to use (SHA-256 in this example)
 $hashAlgorithm = "SHA256"
 
 # Recursively scan files in the specified root directory
 Get-ChildItem -Path $rootDirectory -File -Recurse -ErrorAction SilentlyContinue | ForEach-Object {
     $fileInfo = $_
-    
-    # Calculate file hash
-    $hash = Get-FileHash -Path $fileInfo.FullName -Algorithm $hashAlgorithm
-    
-    # Print file name and hash
-    "$($fileInfo.FullName), $($hash.Hash)"
+
+    # Check if the file's directory is in the excluded list
+    $isExcluded = $excludedDirectories -contains $fileInfo.Directory.FullName
+
+    if (-not $isExcluded) {
+        # Calculate file hash
+        $hash = Get-FileHash -Path $fileInfo.FullName -Algorithm $hashAlgorithm
+
+        # Print file name and hash
+        "$($fileInfo.FullName), $($hash.Hash)"
+    }
 }

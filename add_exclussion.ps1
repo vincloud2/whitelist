d56d0572aac19e52884537247f4e28c92432ea83
vincloud2 <128999303+vincloud2@users.noreply.github.com>
2023-08-10 00:50:24 +0530

    Update add_exclussion.ps1

--- add_exclussion.ps1
+++ add_exclussion.ps1
@@ -1,15 +1,15 @@
 # Set the path to the CSV file containing the list of files to exclude
-$csvFilePath = "C:\whitelist\whitelist\output.csv"
+$csvFileName = "C:\whitelist\whitelist\output.csv"
 
 # Read the CSV file and get the list of files to exclude
-$filesToExclude = Import-Csv -Path $csvFilePath
+$filesToExclude = Import-Csv -Path $csvFileName
 
 # Add each file in the list as an exclusion
 foreach ($fileEntry in $filesToExclude) {
-    $filePath = $fileEntry.FilePath
-    Write-Host "Adding exclusion for: $filePath"
+    $fileName = $fileEntry.FileName
+    Write-Host "Adding exclusion for: $fileName"
     
-    Add-MpPreference -ExclusionPath $filePath
+    Add-MpPreference -ExclusionPath $fileName
 }
 
 Write-Host "Exclusion process completed."
